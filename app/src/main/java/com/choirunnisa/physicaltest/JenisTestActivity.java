c4131702ec96baebe1bcaeddd0f3986bf1ebc243
Choirunnisa1 <nisaninda3@gmail.com>
2019-12-26 09:53:06 +0700

    bismillah

---
+++ app/src/main/java/com/choirunnisa/physicaltest/JenisTestActivity.java
@@ -0,0 +1,14 @@
+package com.choirunnisa.physicaltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JenisTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jenis_test);
+    }
+}
---
+++ app/src/main/java/com/choirunnisa/physicaltest/LihatDataActivity.java
@@ -0,0 +1,14 @@
+package com.choirunnisa.physicaltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LihatDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_data);
+    }
+}
